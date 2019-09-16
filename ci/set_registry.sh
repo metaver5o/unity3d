4356bb9feaaebe1883c38bec1dc209e086f42222
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-09-15 21:45:35 -0400

    Update ci-generator to support branches correctly

---
+++ ci/set_registry.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -ex
+if [ "$CI_COMMIT_REF_NAME" = "master" ]; then
+  export REGISTRY=$CI_REGISTRY_IMAGE:$TAG
+else
+  export REGISTRY=$CI_REGISTRY_IMAGE:$TAG-$CI_COMMIT_REF_SLUG
+fi
