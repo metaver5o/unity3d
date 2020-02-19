cec1b74e57a62e9af8c44d0303f2846e306aee63
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2020-02-19 00:57:57 -0500

    uncomment tests from previous commit

--- ci-generator/tests/test_check_new_version.py
+++ ci-generator/tests/test_check_new_version.py
@@ -62,44 +62,44 @@ class TestGitlabCiGenerator(TestCase):
         self.assertEqual(expected_response, response)
 
     def test_generate_unity_version_block(self):
-        # self.maxDiff = None
-        # release_key = 'official'
-        # releases_source = 'data/releases-linux-2019-05-30.json'
-        # release_index = 0
-        # with self.subTest(f'{releases_source} {release_key}[{release_index}]'):
-        #     expected_release_result = {
-        #         '2017.4.27f1': {
-        #             'build': 'f1',
-        #             'dockerfile_name': 'unitysetup',
-        #             'download_url': 'https://beta.unity3d.com/download/0c4b856e4c6e/UnitySetup-2017.4.27f1',
-        #             'release_notes': 'https://unity3d.com/unity/whats-new/2017.4.27f1',
-        #             'release_url': 'https://beta.unity3d.com/download/0c4b856e4c6e/public_download.html',
-        #             'sha1': '8dae4dd18df383a598830c6e2489cdecdcb19273',
-        #             'underscore': '2017_4_27f1',
-        #             'version': '2017.4.27'
-        #         }
-        #     }
-        #     self.do_test_generate_unity_version_block(releases_source, release_key, release_index,
-        #                                               expected_release_result)
-        #
-        # release_key = 'beta'
-        # releases_source = 'data/releases-linux-2020-02-18.json'
-        # release_index = 0
-        # with self.subTest(f'{releases_source} {release_key}[{release_index}]'):
-        #     expected_release_result = {
-        #         '2020.1.0a23': {
-        #             'build': 'a23',
-        #             'dockerfile_name': 'unitysetup',
-        #             'download_url': 'https://beta.unity3d.com/download/607f55d6e9ce/UnitySetup-2020.1.0a23',
-        #             'release_notes': 'https://unity3d.com/unity/whats-new/2020.1.0a23',
-        #             'release_url': 'https://beta.unity3d.com/download/607f55d6e9ce/public_download.html',
-        #             'sha1': '03376c0669c4e48e13ab3ae9d54a0e0e07294906',
-        #             'underscore': '2020_1_0a23',
-        #             'version': '2020.1.0'
-        #         }
-        #     }
-        #     self.do_test_generate_unity_version_block(releases_source, release_key, release_index,
-        #                                               expected_release_result)
+        self.maxDiff = None
+        release_key = 'official'
+        releases_source = 'data/releases-linux-2019-05-30.json'
+        release_index = 0
+        with self.subTest(f'{releases_source} {release_key}[{release_index}]'):
+            expected_release_result = {
+                '2017.4.27f1': {
+                    'build': 'f1',
+                    'dockerfile_name': 'unitysetup',
+                    'download_url': 'https://beta.unity3d.com/download/0c4b856e4c6e/UnitySetup-2017.4.27f1',
+                    'release_notes': 'https://unity3d.com/unity/whats-new/2017.4.27f1',
+                    'release_url': 'https://beta.unity3d.com/download/0c4b856e4c6e/public_download.html',
+                    'sha1': '8dae4dd18df383a598830c6e2489cdecdcb19273',
+                    'underscore': '2017_4_27f1',
+                    'version': '2017.4.27'
+                }
+            }
+            self.do_test_generate_unity_version_block(releases_source, release_key, release_index,
+                                                      expected_release_result)
+
+        release_key = 'beta'
+        releases_source = 'data/releases-linux-2020-02-18.json'
+        release_index = 0
+        with self.subTest(f'{releases_source} {release_key}[{release_index}]'):
+            expected_release_result = {
+                '2020.1.0a23': {
+                    'build': 'a23',
+                    'dockerfile_name': 'unitysetup',
+                    'download_url': 'https://beta.unity3d.com/download/607f55d6e9ce/UnitySetup-2020.1.0a23',
+                    'release_notes': 'https://unity3d.com/unity/whats-new/2020.1.0a23',
+                    'release_url': 'https://beta.unity3d.com/download/607f55d6e9ce/public_download.html',
+                    'sha1': '03376c0669c4e48e13ab3ae9d54a0e0e07294906',
+                    'underscore': '2020_1_0a23',
+                    'version': '2020.1.0'
+                }
+            }
+            self.do_test_generate_unity_version_block(releases_source, release_key, release_index,
+                                                      expected_release_result)
 
         release_key = 'official'
         releases_source = 'data/releases-linux-2020-02-18.json'
